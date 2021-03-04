dfd8c844035f7df5b7edf54cfdc487e4caba8618
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-03-04 10:05:19 +0000

    Added groupee input methods

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 # Grouper - App for creating sub-groups of people
 
+import random
+
 def main():
 	pass
+
+
+class Grouper:
+	def __init__(self):
+		self.groupee_list = []
+		self.group_size = 3
+		self.groups = []
+
+	def add_groupee(self, name):
+		self.groupee_list.append(name)
+
+	def remove_groupee(self, name):
+		if name in self.groupee_list:
+			self.groupee_list.remove(name)
+
+	def create_groups(self):
+		group_count = len(self.groupee_list) // self.group_size
+
+		random.shuffle(self.groupee_list)
+		index = 0
+		for groupee in self.groupee_list:
+			self.groups[index % group_count].append(groupee)
+			index += 1
+
 
 if __name__ == "__main__":
 	main()

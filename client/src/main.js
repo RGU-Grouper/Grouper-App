9d824c364422a161a033ec973c04abac348edfcc
PatrickJMurchie <patrickjohnmurchie@gmail.com>
2021-03-11 13:21:32 +0000

    Still things to do, need to get the groups outputted from createGroups function

--- client/src/main.js
+++ client/src/main.js
@@ -9,27 +9,87 @@ const removeUser = (name) => {
 };
 
 // Set minimum users per group
-const setMinimumSize = (size) => {
+function setMinimumSize() {
     minSize = document.getElementById("minSize").value;
-    console.log(minSize); 
+    return minSize 
 };
 
-// Assign users to groups
+//split names function
+function splitNames(){
+
+    //Get groups from text box
+   var str = document.getElementById("groupNames").value;
+   //Splits groups up each space
+   var names = str.split(" ");
+
+   //Returns names
+   return names;
+};
+
+// Assign users to groups THIS IS NOT WORKING YET AND IS A MESS, Just put it in so people could help
 const createGroups = () => {
 
-    var str = document.getElementById("groupNames").value;
-    var names = str.split(" ");
+    //add names to a variable
+    var n = splitNames()
     
-    console.log(names); 
+    //adds minSize to a variable
+    var ms = setMinimunSize()
 
-    groupNumber = Math.floor(names.length / setMinimunSize.minSize)
+    //Works out the number of groups
+    groupNumber = Math.floor(n.length / ms)
     
-    var i;
-    for (i = 0; i < groupNumber; i++){
-        current = Math.floor(Math.random() * names.length); 
-        group.push(names[current])
+    //counters
+    var counter = 0;
+    
+
+    //Variable to say if the groupNumber is even or odd, this is set to false each loop
+    var even = false;
+
+    //Creates array that is going to be a matrix with all the groups in it
+    var groups = [];
+
+    //makes sure to add remainders to a group
+    if (groupNumber < n.length / ms & even == false){
+
+        //counter
+        var i;
+        //Adds people to group and the extra person left over
+        for (i = 0; i < groupNumber; i++){
+
+            for (i2 = 0; i2 < ms + 1; i2++){
+                current = Math.floor(Math.random() * n.length); 
+    
+                //Creates new array in matrix
+                groups[counter] = new Array(counter);
+
+                //Adds names to the array in the matrix
+                groups[counter.push(n[current])];
+
+                //Adds one to counter
+                counter = counter + 1;
+                even = true
+            }
+        }
+    } else {
+        //counter
+        var i;
+        //Adds people to groups if it is even
+        for (i = 0; i < groupNumber; i++){
+
+            for (i2 = 0; i2 < ms + 1; i2++){
+                current = Math.floor(Math.random() * n.length); 
+    
+                //Creates new array in matrix
+                groups[counter] = new Array(counter);
+
+                //Adds names to the array in the matrix
+                groups[counter.push(n[current])];
+
+                //Adds one to counter
+                counter = counter + 1;
+            }  
+        }
+        
     }
-
+    console.log(groups)
 } ;
-
-// CHANGES  

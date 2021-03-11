1bcf11e87d74973c5f1ea13cf7cba662418f55e1
PatrickJMurchie <patrickjohnmurchie@gmail.com>
2021-03-11 11:51:55 +0000

    mess

--- client/src/main.js
+++ client/src/main.js
@@ -10,18 +10,26 @@ const removeUser = (name) => {
 
 // Set minimum users per group
 const setMinimumSize = (size) => {
-
+    minSize = document.getElementById("minSize").value;
+    console.log(minSize); 
 };
 
 // Assign users to groups
 const createGroups = () => {
 
-    group = [];
+    var str = document.getElementById("groupNames").value;
+    var names = str.split(" ");
+    
+    console.log(names); 
+
+    groupNumber = Math.floor(names.length / setMinimunSize.minSize)
     
     var i;
-for (i = 0; i < group.length; i++) {
-    group.push("");
-} 
+    for (i = 0; i < groupNumber; i++){
+        current = Math.floor(Math.random() * names.length); 
+        group.push(names[current])
+    }
 
-};
+} ;
+
 // CHANGES  

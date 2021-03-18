011a1449e38ed22a7ce811cca4aceefefa6a76f8
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-03-18 11:23:14 +0000

    Added comment

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
+// Entry Point
+
 import { DOM, addUser } from './ui.js';
 import { createGroups } from './groups.js';
 
 // Set UI callbacks
-DOM.createGroupsForm.onsubmit = e => e.preventDefault();
+DOM.createGroupsForm.onsubmit = event => event.preventDefault();
 DOM.createGroupsButton.onclick = createGroups;
 DOM.userListButton.onclick = addUser;

1fcb0e90548cab4bd86c3bae4b01382890df0e7b
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-03-18 00:48:06 +0000

    Changed createGroups()

--- client/src/main.js
+++ client/src/main.js
@@ -1,95 +1,104 @@
-// Add user to list
-const addUser = (name) => {
-
+const DOM = {
+	minSize: document.getElementById("minSize"),
+	groupNames: document.getElementById("groupNames"),
+	createGroupsForm: document.querySelector(".create-groups"),
+	createGroupsButton: document.querySelector(".create-groups__button"),
 };
 
-// Remove user from list
-const removeUser = (name) => {
+const shuffle = (array) => {
+	let currentIndex = array.length;
 
+  while (currentIndex !== 0) {
+		const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    const temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
 };
 
-// Set minimum users per group
-function setMinimumSize() {
-    minSize = document.getElementById("minSize").value;
-    return minSize 
-};
+// Get minimum users per group from user input
+const getMinimumSize = () => DOM.minSize.value;
 
-//split names function
-function splitNames(){
+// Get names of potential group members from user input
+const getNameList = () => { 
+	//Get user list from text box as string
+	const nameString = DOM.groupNames.value;
+	
+	// Split string of names into a list
+	const nameList = nameString.split(" ");
 
-    //Get groups from text box
-   var str = document.getElementById("groupNames").value;
-   //Splits groups up each space
-   var names = str.split(" ");
+	// Shuffle the list of names into a random order
+	shuffle(nameList);
 
-   //Returns names
-   return names;
+	// Returns list of names
+	return nameList;
 };
 
 // Assign users to groups THIS IS NOT WORKING YET AND IS A MESS, Just put it in so people could help
 const createGroups = () => {
+	const nameList = getNameList();
+	const minSize = getMinimumSize();
 
-    //add names to a variable
-    var n = splitNames()
-    
-    //adds minSize to a variable
-    var ms = setMinimunSize()
+	// Calculate the number of groups
+	const groupCount = Math.floor(nameList.length / minSize);
 
-    //Works out the number of groups
-    groupNumber = Math.floor(n.length / ms)
-    
-    //counters
-    var counter = 0;
-    
+	// Matrix to hold all groups
+	const groups = [];
 
-    //Variable to say if the groupNumber is even or odd, this is set to false each loop
-    var even = false;
+	nameList.forEach((name, index) => {
+		const groupIndex = index % groupCount;
 
-    //Creates array that is going to be a matrix with all the groups in it
-    var groups = [];
+		if (groups[groupIndex]) {
+			groups[groupIndex].push(name);
+		}
+		else {
+			groups[groupIndex] = [name];
+		}
+	});
 
-    //makes sure to add remainders to a group
-    if (groupNumber < n.length / ms & even == false){
+	// Display groups
+	console.log(groups)
+};
+	
+// Set UI callbacks
+DOM.createGroupsForm.onsubmit = e => e.preventDefault();
+DOM.createGroupsButton.onclick = createGroups;
 
-        //counter
-        var i;
-        //Adds people to group and the extra person left over
-        for (i = 0; i < groupNumber; i++){
 
-            for (i2 = 0; i2 < ms + 1; i2++){
-                current = Math.floor(Math.random() * n.length); 
-    
-                //Creates new array in matrix
-                groups[counter] = new Array(counter);
+// //makes sure to add remainders to a group
+// if (groupNumber < nameList.length / minSize && even == false){
+// 	//Adds people to group and the extra person left over
+// 	for (let i = 0; i < groupNumber; i++){
+// 		for (let j = 0; j < minSize + 1; j++){
+// 			const current = Math.floor(Math.random() * nameList.length); 
 
-                //Adds names to the array in the matrix
-                groups[counter.push(n[current])];
+// 			//Creates new array in matrix
+// 			groups[counter] = new Array(counter);
 
-                //Adds one to counter
-                counter = counter + 1;
-                even = true
-            }
-        }
-    } else {
-        //counter
-        var i;
-        //Adds people to groups if it is even
-        for (i = 0; i < groupNumber; i++){
+// 			//Adds nameList to the array in the matrix
+// 			groups[counter.push(nameList[current])];
 
-            for (i2 = 0; i2 < ms + 1; i2++){
-                current = Math.floor(Math.random() * n.length); 
-    
-                //Creates new array in matrix
-                groups[counter] = new Array(counter);
+// 			//Adds one to counter
+// 			counter = counter + 1;
+// 			even = true
+// 		}
+// 	}
+// } else {
+// 	//Adds people to groups if it is even
+// 	for (let i = 0; i < groupNumber; i++){
+// 		for (let j = 0; j < minSize + 1; j++){
+// 			const current = Math.floor(Math.random() * nameList.length); 
+	
+// 			//Creates new array in matrix
+// 			groups[counter] = new Array(counter);
 
-                //Adds names to the array in the matrix
-                groups[counter.push(n[current])];
+// 			//Adds names to the array in the matrix
+// 			groups[counter.push(nameList[current])];
 
-                //Adds one to counter
-                counter = counter + 1;
-            }  
-        }
-        
-    }
-    console.log(groups)
-} ;
+// 			//Adds one to counter
+// 			counter = counter + 1;
+// 		}  
+// 	}
+// }
